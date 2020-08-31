32f1aeda007221d0e415aca049047d8f7af725b3
notrealjohn <johnc33594@gmail.com>
2020-08-31 16:11:35 -0400

    initial create-app to test with heroku

---
+++ website/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div className="main-page">
+        <header className="main-page-header">
+          <p> This is my website</p>
+        </header>
+      </div>
+  );
+}
+
+export default App;
